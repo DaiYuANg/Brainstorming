415ac442f48838c9acef5135c6968c4dbd4a5154
DaiYuANg <138393979+DaiYuANg@users.noreply.github.com>
2023-08-24 00:18:46 +0800

    update commont config

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,27 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket_okapi::swagger_ui::{make_swagger_ui, SwaggerUIConfig};
+
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+fn get_docs() -> SwaggerUIConfig {
+    use rocket_okapi::swagger_ui::UrlObject;
+
+    SwaggerUIConfig {
+        url: "/my_resource/openapi.json".to_string(),
+        urls: vec![UrlObject::new("My Resource", "/v1/company/openapi.json")],
+        ..Default::default()
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/swagger", make_swagger_ui(&get_docs()))
+        .mount("/", routes![index])
 }
