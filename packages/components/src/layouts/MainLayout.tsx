0344ff05ee32e7755b902a99ad57660b9a4b7815
DaiYuANg <dai_yuang@icloud.com>
2023-09-13 18:57:58 +0800

    format

--- packages/components/src/layouts/MainLayout.tsx
+++ packages/components/src/layouts/MainLayout.tsx
@@ -1,15 +1,28 @@
-import {AppShell, Header, Navbar} from '@mantine/core';
+import { AppShell, Header, Navbar } from '@mantine/core';
 
 const MainLayout = () => {
   return (
     <>
       <AppShell
-          padding="md"
-          navbar={<Navbar width={{ base: 300 }} height={500} p="xs">{/* Navbar content */123}</Navbar>}
-          header={<Header height={60} p="xs">{/* Header content */312}</Header>}
-          styles={(theme) => ({
-              main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
-          })}
+        padding="md"
+        navbar={
+          <Navbar width={{ base: 300 }} height={500} p="xs">
+            {/* Navbar content */ 123}
+          </Navbar>
+        }
+        header={
+          <Header height={60} p="xs">
+            {/* Header content */ 312}
+          </Header>
+        }
+        styles={(theme) => ({
+          main: {
+            backgroundColor:
+              theme.colorScheme === 'dark'
+                ? theme.colors.dark[8]
+                : theme.colors.gray[0],
+          },
+        })}
       >
         <div>dasda</div>
       </AppShell>
