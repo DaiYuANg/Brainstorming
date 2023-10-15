8f47ced269b3570ae36703e9877c81783dd2e532
DaiYuANg <138393979+DaiYuANg@users.noreply.github.com>
2023-10-15 21:26:54 +0800

    try lazy component

--- packages/desktop/src/App.tsx
+++ packages/desktop/src/App.tsx
@@ -1,13 +1,14 @@
 import { MantineProvider } from '@mantine/core';
-import { MainLayout } from './components/layout';
-
 import { lazy } from 'react';
+import { MainLayout } from './components/layout';
 import { theme } from './theme.ts';
+
 const ApplicationSpotlight = lazy(() =>
   import('./components/ApplicationSpotlight.tsx').then(
     ({ ApplicationSpotlight }) => ({ default: ApplicationSpotlight }),
   ),
 );
+
 const ContextMenu = lazy(() =>
   import('./components/ContextMenu.tsx').then(({ ContextMenu }) => ({
     default: ContextMenu,
