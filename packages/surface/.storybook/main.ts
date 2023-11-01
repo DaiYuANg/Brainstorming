cc505b0f2205783ab8b5f4e46211a75103e6e495
DaiYuANg <138393979+DaiYuANg@users.noreply.github.com>
2023-11-02 01:04:37 +0800

    update surface package.json and add some storybook plugin

--- packages/surface/.storybook/main.ts
+++ packages/surface/.storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import * as path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -7,6 +8,21 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
+    'storybook-dark-mode',
+    '@storybook/addon-a11y',
+    {
+      name: '@storybook/addon-storysource',
+      options: {
+        rule: {
+          // test: [/\.stories\.jsx?$/], This is default
+          include: [path.resolve(__dirname, '../src')], // You can specify directories
+        },
+        loaderOptions: {
+          prettierConfig: { printWidth: 80, singleQuote: false },
+        },
+      },
+    },
+    'storybook-addon-pseudo-states',
   ],
   framework: {
     name: '@storybook/react-vite',
