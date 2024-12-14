de806a4c830712dd1b9b602c7bb689935b951074
DaiYuANg <138393979+DaiYuANg@users.noreply.github.com>
2024-12-15 01:18:18 +0800

    update readme and add license

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Brainstorming
 
-- 个人文本区
+> Current is experimental
+
+Brainstorming is an open-source organized document writer application
+
+## Activity
+
+![Alt](https://repobeats.axiom.co/api/embed/8e6a000bce59dbc34b7dd6ac5d33c157eab0125a.svg 'Repobeats analytics image')
+
+## License
+
+[License](./LICENSE.txt)
--- packages/desktop/src-tauri/src/command.rs
+++ packages/desktop/src-tauri/src/command.rs
@@ -1,8 +1,7 @@
+use data_structure::element::Document;
 use std::fs::File;
 use std::io::Write;
-use data_structure::element::Document;
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 pub fn greet(name: &str) -> String {
   format!("Hello, {}! You've been greeted from Rust!", name)
@@ -10,13 +9,13 @@ pub fn greet(name: &str) -> String {
 
 fn save_document_to_file(document: &Document, path: &str) -> std::io::Result<()> {
   // 将文档序列化为二进制数据
-  let encoded: usize = bincode::encode_into_std_write(document).unwrap();
+  let serialized = serde_json::to_value(&document)?.to_string();
 
   // 打开文件（如果不存在会创建）
   let mut file = File::create(path)?;
 
   // 将二进制数据写入文件
-  // file.write_all(&encoded)?;
+  file.write_all((&serialized).as_ref())?;
 
   Ok(())
 }
